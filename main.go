4112b1d0e2aad812bc8daaf273e60f88ad0fd277
Angus Lee <angusmhlee@gmail.com>
2023-11-12 20:16:23 +0800

    Simple event bus

--- main.go
+++ main.go
@@ -1,7 +1,87 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type DataEvent struct {
+	Data interface{}
+	Topic string
+ }
+
+type DataChannel chan DataEvent
+
+type DataChannelSlice []DataChannel
+
+type EventBus struct {
+	subscribers map[string]DataChannelSlice
+	rm sync.RWMutex
+}
+
+//Subscribe to a topic
+func (eb *EventBus) Subscribe(topic string, ch DataChannel) {
+	eb.rm.Lock()
+	if prev, found := eb.subscribers[topic]; found {
+		eb.subscribers[topic] = append(prev, ch)
+	} else{
+		eb.subscribers[topic] = append([]DataChannel{}, ch)
+	}
+	eb.rm.Unlock()
+}
+
+//Publish a topic to the bus
+func (eb *EventBus) Publish(topic string, data interface{}) {
+	eb.rm.RLock()
+	if chans, found := eb.subscribers[topic]; found {
+		channels := append(DataChannelSlice{}, chans...)
+		go func(data DataEvent, dataChannels DataChannelSlice) {
+			for _, ch := range dataChannels {
+				ch <- data
+			}
+		}(DataEvent{data, topic}, channels)
+	}
+	eb.rm.RUnlock()
+}
+
+var eb = &EventBus{
+	subscribers: map[string]DataChannelSlice{},
+}
+
+func printDataEvent(ch string, data DataEvent) {
+	fmt.Printf("Channel: %s; Topic: %s; DataEvent: %v\n", ch, data.Topic, data.Data)
+}
+
+func publisTo(topic string, data string) {
+	for {
+		eb.Publish(topic, data)
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	ch1 := make(chan DataEvent)
+	ch2 := make(chan DataEvent)
+	ch3 := make(chan DataEvent)
+
+	eb.Subscribe("topic1", ch1)
+	eb.Subscribe("topic2", ch2)
+	eb.Subscribe("topic3", ch3)
+
+	go publisTo("topic1", "Hi topic 1")
+	go publisTo("topic2", "Welcome to topic 2")
+	go publisTo("topic3", "Welcome to topic 3")
+
+	for {
+		select {
+		case d := <-ch1:
+			go printDataEvent("ch1", d)
+		case d := <-ch2:
+			go printDataEvent("ch2", d)
+		case d := <-ch3:
+			go printDataEvent("ch3", d)
+		}
+	}
 }
